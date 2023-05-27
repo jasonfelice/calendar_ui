034ce1f89d216b88d03341206637cd583cbf8816
Jake Felice <jasonfelice0@gmail.com>
2023-05-27 12:59:24 +0530

    Set up Range Calendar layout

---
+++ Calendar UI/Controllers/RangeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calendar_UI.Controllers
+{
+    public class RangeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
